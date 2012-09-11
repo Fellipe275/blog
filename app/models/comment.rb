7019a36c602aa274d97949b99bc072ab11dcdb19
fellipe <Fellipe275@hotmail.com>
2012-09-11 18:35:02 -0300

    Projeto de blog

---
+++ app/models/comment.rb
@@ -0,0 +1,18 @@
+class Comment < ActiveRecord::Base
+  attr_accessible :article_id, :body, :email, :name
+  belongs_to :article
+
+  validates :name, :body, :email, :presence => true
+
+  validate :article_should_be_published
+
+
+  def article_should_be_published
+
+  	errors.add(:article_id, "is not published yet") if article && !article.published?
+
+  end
+
+ 
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '160d93d09e015af23578733382a695e9b3d1d110d4f538b84536da4e40e50db85813c26aa8fb3a9d80a5694e2236744db0950962fba754d8bbebe7036b9e54ce'
